06e305cc193a570048af27a431abd9d716e90b20
Erisa Dervishi <erisa85d@gmail.com>
2015-05-27 15:46:30 -0400

    Seems to be working main algorithm, but it is still ugly

--- game.js
+++ game.js
@@ -2,15 +2,15 @@ var width = 10;
 var height = 10;
 
 var board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			 [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-			 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			 [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-			 [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-			 [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-			 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+	 [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+	 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	 [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+	 [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+	 [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+	 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
 
 function printBoard(board) {
 	for (var i = 0; i < height; i++){
@@ -32,3 +32,22 @@ function countAliveAdjacentCells(x,y){
 			return count;
 };
 
+function processNewBoard(board){
+	var newBoard = board;
+	for (var i = 0; i < height; i++)
+		for (var j= 0; j < width; j++)
+			newBoard[i][j] = processCell(board[i][j], countAliveAdjacentCells(i, j));
+	return newBoard;
+}
+
+function processCell(currentState, count){
+	if ((currentState == 1 && count <= 3 && count >= 2) ||
+		(currentState == 0 && count == 3))
+		return 1;
+	return 0;
+}
+
+function MainLoop() {
+	board = processNewBoard(board);
+	printBoard(board);
+}
